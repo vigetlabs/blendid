b5ad649f4a1f60beb79ce5287833189b883bbc77
Dan Tello <dan.tello@gmail.com>
2014-03-31 20:50:07 -0400

    Remove Audio tasks and files

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -5,6 +5,5 @@ module.exports = function(){
 	gulp.watch('src/javascript/**/*', ['browserify']);
 	gulp.watch('src/sass/**', ['compass']);
 	gulp.watch('src/images/**', ['images']);
-	gulp.watch('src/audio/**', ['audio']);
 	livereload();
 };
--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 // Intitialize Gulp with each task we plan on using. This
 // runs the method defined gulp/index.js, which returns gulp.
 var gulp = require('./gulp')([
-	'audio',
 	'browserify',
 	'compass',
 	'images',
@@ -11,7 +10,7 @@ var gulp = require('./gulp')([
 ]);
 
 // Create any stand alone batch tasks you'd like to use
-gulp.task('build', ['audio', 'browserify', 'compass', 'images']);
+gulp.task('build', ['browserify', 'compass', 'images']);
 
 // Create your default task that gets run with the `gulp` command
 gulp.task('default', ['build', 'watch', 'serve', 'open']);
