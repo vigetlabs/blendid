500812a4b131af9cd22a0ba66f1aabfcf50d1681
Dan Tello <dan.tello@gmail.com>
2014-03-31 21:37:47 -0400

    remove excess comments

--- gulp/index.js
+++ gulp/index.js
@@ -2,9 +2,8 @@ var gulp = require('gulp');
 
 module.exports = function(tasks) {
 	tasks.forEach(function(name) {
-		// Initialize individual tasks
 		gulp.task(name, require('./tasks/' + name));
 	});
-	
+
 	return gulp;
 };
--- gulp/tasks/browserify.js
+++ gulp/tasks/browserify.js
@@ -6,24 +6,16 @@ var source = require('vinyl-source-stream');
 
 module.exports = function() {
 	browserify({
-		// The single point of entry for our app
-		entries: ['./src/javascript/app.coffee'],
-		// addtional file extentions to make optional in your requires
-		extensions: ['.coffee', '.hbs']
-	})
-	// Expose the version of underscore that comes with backbone so we can
-	// require it in our code without includin underscore twice!
-	.require('backbone/node_modules/underscore', { expose: 'underscore' })
-	.bundle({debug: true}) // debug: true enables source mapping!
-	// Send compile errors to notification center
-	.on('error', notify.onError({
-		message: "<%= error.message %>",
-		title: "JavaScript Error"
-	}))
-	// vinyl source stream lets us use your bundle with gulp
-	.pipe(source('app.js'))
-	// Output files with gulp.dest
-	.pipe(gulp.dest('./build/'))
-	// Live reload once we're done
-	.pipe(livereload());
+			entries: ['./src/javascript/app.coffee'],
+			extensions: ['.coffee', '.hbs']
+		})
+		.require('backbone/node_modules/underscore', { expose: 'underscore' })
+		.bundle({debug: true})
+		.on('error', notify.onError({
+			message: "<%= error.message %>",
+			title: "JavaScript Error"
+		}))
+		.pipe(source('app.js'))
+		.pipe(gulp.dest('./build/'))
+		.pipe(livereload());
 };
