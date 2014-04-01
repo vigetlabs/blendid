1aaa103cf88aeb7e1d70f4c11cdbaa9f2b41f7ec
Dan Tello <dan.tello@gmail.com>
2014-03-31 20:05:06 -0400

    Open chrome on server start

--- gulp/tasks/serve.js
+++ gulp/tasks/serve.js
@@ -1,11 +1,22 @@
 var connect = require('connect');
+var gulp    = require("gulp");
 var http    = require('http');
+var open    = require("gulp-open");
 
 module.exports = function(){
 	var buildPath = __dirname.split('/gulp/tasks')[0];
 	var app = connect()
 		.use(connect.logger('dev'))
 		.use(connect.static(buildPath));
-	var server = http.createServer(app);
-	server.listen(8080);
+
+	// Create server
+	http.createServer(app).listen(8080);
+
+	// Open in Chrome
+	var options = {
+		url: "http://localhost:8080",
+		app: "google chrome"
+	};
+
+	gulp.src("./index.html").pipe(open("", options));
 };
