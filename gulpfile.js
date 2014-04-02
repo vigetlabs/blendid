1dba7fbf10420cc314f3f12544476196e0816787
Dan Tello <dan.tello@gmail.com>
2014-04-02 12:39:16 -0400

    Remove some comments

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,3 @@
-// Intitialize Gulp with each task we plan on using. This
-// runs the method defined gulp/index.js, which returns gulp.
 var gulp = require('./gulp')([
 	'browserify',
 	'compass',
@@ -9,8 +7,5 @@ var gulp = require('./gulp')([
 	'serve'
 ]);
 
-// Create any stand alone batch tasks you'd like to use
 gulp.task('build', ['browserify', 'compass', 'images']);
-
-// Create your default task that gets run with the `gulp` command
 gulp.task('default', ['build', 'watch', 'serve', 'open']);
