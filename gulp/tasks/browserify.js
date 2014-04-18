1965a10d8060bc9401299fcd219204c3fd961f2b
Dan Tello <dan.tello@gmail.com>
2014-04-18 15:55:08 -0400

    Filter out possible non-script tasks files from task list.

--- gulp/tasks/browserify.js
+++ gulp/tasks/browserify.js
@@ -1,8 +1,8 @@
-var browserify = require('browserify');
-var gulp       = require('gulp');
-var livereload = require('gulp-livereload');
-var source = require('vinyl-source-stream');
+var browserify   = require('browserify');
+var gulp         = require('gulp');
 var handleErrors = require('../util/handleErrors');
+var livereload   = require('gulp-livereload');
+var source       = require('vinyl-source-stream');
 
 gulp.task('browserify', function(){
 	return browserify({
