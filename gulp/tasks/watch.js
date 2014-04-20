c8a2209887994d9d2c67fb509f2fa3cd05d8bebb
Paul Prince <paul@littlebluetech.com>
2014-04-20 19:57:37 -0400

    Make `watch` run after `build`, not simultaneously.
    This makes the `watch` have the `build` task as a dependency.  While
    this would not be ideal for a large project, I think it should be OK for
    most projects (since gulp is fast; this wouldn't work for me in grunt).
    
    I imagine an end-user of this repo will have much customized the gulp
    configuration anyway by the time their project is large enough for this
    to be a problem.

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -3,7 +3,7 @@ var config     = require('../config');
 var gulp        = require('gulp');
 var browserSync = require('browser-sync');
 
-gulp.task('watch', function() {
+gulp.task('watch', ['build'], function(){
 	gulp.watch('src/sass/**', ['compass']);
 	gulp.watch('src/images/**', ['images']);
 	gulp.watch('src/htdocs/**', ['copy']);
