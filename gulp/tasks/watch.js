30ade462cef5e56be4389239e42f904748c97d8d
Paul Prince <paul@littlebluetech.com>
2014-04-20 18:35:09 -0400

    Add Browser-Sync.
    This replaces:  serve and open tasks, livereload
    
    As of this commit, index.html is not served (because it isn't in
    build/).

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,5 +1,7 @@
 var gulp       = require('gulp');
 var config     = require('../config');
+var gulp        = require('gulp');
+var browserSync = require('browser-sync');
 
 gulp.task('watch', function() {
 	gulp.watch('src/sass/**', ['compass']);
@@ -7,4 +9,10 @@ gulp.task('watch', function() {
 	// Note: Javascript watching is handled by watchify
 	// in gulp/tasks/browserify.js, when this flag is true
 	config.isWatching = true;
+
+	browserSync.init(['build/**'], {
+		server: {
+			baseDir: 'build'
+		}
+	});
 });
