c0618e52a265ed55da5565bf687d9d3435ae8feb
Paul Prince <paul@littlebluetech.com>
2014-04-20 19:32:54 -0400

    Move index.html out of root and add copy task.

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -6,6 +6,7 @@ var browserSync = require('browser-sync');
 gulp.task('watch', function() {
 	gulp.watch('src/sass/**', ['compass']);
 	gulp.watch('src/images/**', ['images']);
+	gulp.watch('src/htdocs/**', ['copy']);
 	// Note: Javascript watching is handled by watchify
 	// in gulp/tasks/browserify.js, when this flag is true
 	config.isWatching = true;
