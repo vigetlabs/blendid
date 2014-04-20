0dd67866d87db6fda34595a11df39a6fa189f845
Paul Prince <paul@littlebluetech.com>
2014-04-20 15:30:14 -0400

    Remove existing livereload config.

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,18 +1,9 @@
 var gulp       = require('gulp');
-var livereload = require('gulp-livereload');
 var config     = require('../config');
 
 gulp.task('watch', function() {
-	var server = livereload();
-
-	var reload = function(file) {
-		server.changed(file.path);
-	};
-
 	gulp.watch('src/sass/**', ['compass']);
 	gulp.watch('src/images/**', ['images']);
-	gulp.watch(['build/**']).on('change', reload);
-
 	// Note: Javascript watching is handled by watchify
 	// in gulp/tasks/browserify.js, when this flag is true
 	config.isWatching = true;
