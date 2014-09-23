4a441b9b5731e0ab5244abb026e8464c29d53b8e
Maxime Rassi <maxime.rassi@gmail.com>
2014-09-22 22:53:28 -0400

    Syntax Fix
    Missing spacing between '='

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp');
 var sass = require('gulp-ruby-sass');
 var handleErrors = require('../util/handleErrors');
-var config=require('../config').sass;
+var config = require('../config').sass;
 
 gulp.task('sass', ['images'], function () {
   return gulp.src(config.src)
