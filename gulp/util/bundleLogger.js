7341a9b5b7688085231f022e63c3f44bbb30d2a2
AlexanderMac <amatsibarov@gmail.com>
2014-12-30 13:38:08 +0700

    Show bundles filesize in browserify task

--- gulp/util/bundleLogger.js
+++ gulp/util/bundleLogger.js
@@ -4,18 +4,36 @@
 */
 
 var gutil        = require('gulp-util');
+var fs           = require('fs');
 var prettyHrtime = require('pretty-hrtime');
 var startTime;
 
+function getFilesize(filename, filedir) {
+  var filestats = fs.statSync(filedir + '/' + filename);
+  var filesize = filestats["size"];
+  if (filesize > 1000000) { // Mb
+    return (filesize / 1000000).toFixed(2) + 'Mb';
+  }
+  if (filesize > 1000) {    // Kb
+    return (filesize / 1000).toFixed(2) + 'Kb';
+  }
+  return filesize + 'b';
+}
+
 module.exports = {
-  start: function(filepath) {
+  start: function(filename) {
     startTime = process.hrtime();
-    gutil.log('Bundling', gutil.colors.green(filepath) + '...');
+    gutil.log('Bundling', gutil.colors.green(filename) + '...');
   },
 
-  end: function(filepath) {
+  end: function(filename, filedir) {
     var taskTime = process.hrtime(startTime);
     var prettyTime = prettyHrtime(taskTime);
-    gutil.log('Bundled', gutil.colors.green(filepath), 'in', gutil.colors.magenta(prettyTime));
+    if (filedir) {
+      var filesize = getFilesize(filename, filedir);
+      gutil.log('Bundled', gutil.colors.green(filename), 'in', gutil.colors.magenta(prettyTime), 'filesize', gutil.colors.magenta(filesize));
+    } else {
+      gutil.log('Bundled', gutil.colors.green(filename), 'in', gutil.colors.magenta(prettyTime));
+    }
   }
 };
