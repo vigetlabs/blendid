64e3af55e91a0bbf7cd1f5ae7b36a9618fd718cf
Dan Tello <dan.tello@gmail.com>
2015-02-19 20:08:21 -0500

    Run `gulp` on postinstall, update readme

--- gulp/tasks/default.js
+++ gulp/tasks/default.js
@@ -2,7 +2,7 @@ var gulp         = require('gulp');
 var gulpSequence = require('gulp-sequence');
 
 gulp.task('default', function(cb) {
-  var development = ['clean', ['iconFont', 'images'], ['sass', 'watchify'], 'html', 'watch', cb ];
+  var development = [['iconFont', 'images'], ['sass', 'watchify'], 'html', 'watch', cb ];
   var production  = ['karma', 'clean', 'build', ['minifyCss', 'uglifyJs'], 'rev', cb];
   var tasks       = (process.env.NODE_ENV === 'production') ? production : development;
   gulpSequence.apply(this, tasks);
