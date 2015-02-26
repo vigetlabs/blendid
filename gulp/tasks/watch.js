d7fdb1bb6396b484ce784755ac7413954ee73f1a
Dan Tello <dan.tello@gmail.com>
2015-02-26 10:34:19 -0500

    Remove unused require in watch.js

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -5,7 +5,6 @@
 
 var gulp     = require('gulp');
 var config   = require('../config');
-var watchify = require('./browserify')
 
 gulp.task('watch', ['watchify','browserSync'], function(callback) {
   gulp.watch(config.sass.src,   ['sass']);
