539335e9dd4a632e2a6c96ee7e2feddc1c2720d5
Adhil Azeez NV <adhilazeeznv@gmail.com>
2015-02-26 14:14:16 +0530

    Use watchify task instead of browserify task in watch task

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,11 +1,11 @@
 /* Notes:
-   - gulp/tasks/browserify.js handles js recompiling with watchify
+   - gulp/tasks/watchify.js handles js recompiling with watchify
    - gulp/tasks/browserSync.js watches and reloads compiled files
 */
 
 var gulp     = require('gulp');
 var config   = require('../config');
-var watchify = require('./browserify')
+var watchify = require('./watchify')
 
 gulp.task('watch', ['watchify','browserSync'], function(callback) {
   gulp.watch(config.sass.src,   ['sass']);
