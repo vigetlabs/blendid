8e47917ac212f1b4116a9b8e338df5cdfc809dd2
Dan Tello <dan.tello@gmail.com>
2015-03-06 17:42:37 -0500

    Open browser with production-sever task

--- gulpfile.js/tasks/production-server.js
+++ gulpfile.js/tasks/production-server.js
@@ -1,16 +1,20 @@
 var gulp     = require('gulp');
 var express  = require('express');
-var config   = require('../../config').server;
+var config   = require('../config').server;
 var gutil    = require('gulp-util');
 var compress = require('compression');
 var logger   = require('morgan');
+var open     = require('open');
 
-gulp.task('server', ['production'], function(){
+gulp.task('production-server',  function() {
+  var url = 'http://localhost:' + config.port;
+
   express()
     .use(compress())
     .use(logger(config.logLevel))
     .use('/', express.static(config.root, config.staticOptions))
     .listen(config.port)
 
-  gutil('production server started on ' + gutil.colors.green('http://localhost:' + config.port));
+  gutil.log('production server started on ' + gutil.colors.green(url));
+  open(url);
 });
