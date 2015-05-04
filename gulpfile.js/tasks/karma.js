b9aa847702578f29c20cac9ee3ee7c08a1893d73
Robert Van Voorhees <rcvanvo@gmail.com>
2015-05-04 17:08:22 -0400

    Adds requested comment for #133

--- gulpfile.js/tasks/karma.js
+++ gulpfile.js/tasks/karma.js
@@ -6,6 +6,9 @@ var karmaTask = function(done) {
     configFile: process.cwd() + '/karma.conf.js',
     singleRun: true
   }, function(exitStatus) {
+    // Karma's return status is not compatible with gulp's streams
+    // See: http://stackoverflow.com/questions/26614738/issue-running-karma-task-from-gulp
+    // or: https://github.com/gulpjs/gulp/issues/587 for more information
     done(exitStatus ? "There are failing unit tests" : undefined);
   });
 };
