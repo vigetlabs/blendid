60090f3416e76944f47962913ef07d7ba1501cf8
Aminul Islam <aminmedia@gmail.com>
2015-05-11 09:12:08 +0600

    added vendor assets

--- gulpfile.js/tasks/build-development.js
+++ gulpfile.js/tasks/build-development.js
@@ -2,5 +2,5 @@ var gulp         = require('gulp');
 var gulpSequence = require('gulp-sequence');
 
 gulp.task('build:development', function(cb) {
-  gulpSequence('clean', ['fonts', 'iconFont', 'images'], ['sass', 'webpack:development', 'html'], ['watch', 'browserSync'], cb);
+  gulpSequence('clean', ['fonts', 'iconFont', 'vendor', 'images'], ['sass', 'webpack:development', 'html'], ['watch', 'browserSync'], cb);
 });
