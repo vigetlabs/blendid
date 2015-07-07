ce1bade8e669609f8512585ed2454b5eea204af9
Ralf Richtsteiger <dev@rwam.de>
2015-07-07 15:51:28 +0200

    Use iconFont as dependency to avoid build errors on production

--- gulp/tasks/production.js
+++ gulp/tasks/production.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp');
 
 // Run this to compress all the things!
-gulp.task('production', ['karma'], function(){
+gulp.task('production', ['karma', 'iconFont'], function() {
   // This runs only if the karma tests pass
-  gulp.start(['markup', 'images', 'iconFont', 'minifyCss', 'uglifyJs'])
+  gulp.start(['markup', 'images', 'minifyCss', 'uglifyJs']);
 });
