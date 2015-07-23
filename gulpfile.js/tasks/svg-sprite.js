1d410759040e135b9757f57b6404f1873d43f82e
Dan Tello <dan.tello@gmail.com>
2015-07-23 19:24:28 -0400

    Improve svg sprite examples

--- gulpfile.js/tasks/svg-sprite.js
+++ gulpfile.js/tasks/svg-sprite.js
@@ -1,11 +1,15 @@
-var gulp             = require('gulp');
-var imagemin         = require('gulp-imagemin');
-var svgstore         = require('gulp-svgstore');
-var config           = require('../config/svg-sprite');
+var browserSync = require('browser-sync');
+var config      = require('../config/svg-sprite');
+var gulp        = require('gulp');
+var imagemin    = require('gulp-imagemin');
+var svgstore    = require('gulp-svgstore');
 
 gulp.task('svg-sprite', function() {
   return gulp.src(config.src)
     .pipe(imagemin())
     .pipe(svgstore())
-    .pipe(gulp.dest(config.dest));
+    .pipe(gulp.dest(config.dest))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
 });
--- src/javascripts/asyncModules/Highlight.js
+++ src/javascripts/asyncModules/Highlight.js
@@ -2,11 +2,9 @@ export default class Highlight {
   constructor(el) {
     this.el = el
     this.doit()
-    this.el.classList.add('initialized')
   }
 
   doit() {
-    this.el.style.backgroundColor = '#eb4a4c'
-    this.el.style.color = '#fff'
+    this.el.classList.add('highlight')
   }
 }
