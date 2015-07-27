c5a7d9c9442b39a8f88d209154d62a3e14dd7b36
Dan Tello <dan.tello@gmail.com>
2015-07-27 14:45:55 -0400

    Use system temp dir for ghPages temp files

--- gulpfile.js/config/deploy.js
+++ gulpfile.js/config/deploy.js
@@ -1,6 +1,12 @@
 var config = require('./')
+var os = require('os')
+var project = require('../../package.json')
 
 module.exports = {
   url: 'http://greypants.github.io/gulp-starter/',
-  src: config.publicDirectory + '/**/*'
+  src: config.publicDirectory + '/**/*',
+  ghPages: {
+    // https://github.com/shinnn/gulp-gh-pages/issues/63
+    cacheDir: os.tmpdir() + project.name
+  }
 }
