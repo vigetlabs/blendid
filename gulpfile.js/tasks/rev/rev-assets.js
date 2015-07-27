0253c779bc53ef3263921b52af3e201cdff66523
Tim Goshinski <tim@code-chimp.com>
2015-07-26 22:34:58 -0500

    gulp-rev-napkin cleans up unhashed files after rev'ing

--- gulpfile.js/tasks/rev/rev-assets.js
+++ gulpfile.js/tasks/rev/rev-assets.js
@@ -1,7 +1,8 @@
-var config = require('../../config');
+var config         = require('../../config');
 var iconFontConfig = require('../../config/iconFont');
-var gulp   = require('gulp');
-var rev    = require('gulp-rev');
+var gulp           = require('gulp');
+var rev            = require('gulp-rev');
+var revNapkin      = require('gulp-rev-napkin');
 
 // 1) Add md5 hashes to assets referenced by CSS and JS files
 gulp.task('rev-assets', function() {
@@ -11,6 +12,7 @@ gulp.task('rev-assets', function() {
   return gulp.src([config.publicDirectory + '/**/*', notThese])
     .pipe(rev())
     .pipe(gulp.dest(config.publicDirectory))
+    .pipe(revNapkin({verbose: false}))
     .pipe(rev.manifest('public/rev-manifest.json', {merge: true}))
     .pipe(gulp.dest(''));
 });
