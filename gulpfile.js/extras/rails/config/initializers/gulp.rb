3660e99aa6443d0294e6835c76c7c288a9907b6e
Dan Tello <dan.tello@gmail.com>
2015-09-30 10:58:50 -0400

    [update] rails extras

--- gulpfile.js/extras/rails/config/initializers/gulp.rb
+++ gulpfile.js/extras/rails/config/initializers/gulp.rb
@@ -1,6 +1,6 @@
 GULP_CONFIG = JSON.parse(File.read('gulpfile.js/config.json'))
-rev_manifest_path = File.join(GULP_CONFIG['root']['dest'], 'rev-manifest.json')
+REV_MANIFEST_PATH = File.join(GULP_CONFIG['root']['dest'], 'rev-manifest.json')
 
-if File.exist?(rev_manifest_path)
-  REV_MANIFEST = JSON.parse(File.read(rev_manifest_path))
+if File.exist?(REV_MANIFEST_PATH)
+  REV_MANIFEST = JSON.parse(File.read(REV_MANIFEST_PATH))
 end
