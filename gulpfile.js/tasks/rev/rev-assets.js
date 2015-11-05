8637569d349fc18baeaf2db72a8b05fc2598d71f
Dan Tello <dan.tello@gmail.com>
2015-11-05 15:58:07 -0500

    [fix] Don't rev favicons.
    RE: #219

--- gulpfile.js/tasks/rev/rev-assets.js
+++ gulpfile.js/tasks/rev/rev-assets.js
@@ -1,15 +1,29 @@
 var config    = require('../../config')
 var gulp      = require('gulp')
 var path      = require('path')
-var rev       = require('gulp-rev')
-var revNapkin = require('gulp-rev-napkin');
+var rev        = require('gulp-rev')
+var revNapkin  = require('gulp-rev-napkin');
+var assetTasks = require('../../lib/getEnabledTasks')('production').assetTasks
+var uniq       = require('lodash/array/uniq')
+
+var destExtensionPairs = {}
+assetTasks.forEach(function(taskName) {
+  var taskConfig = config.tasks[taskName]
+  destExtensionPairs[taskConfig.dest] = destExtensionPairs[taskConfig.dest] || []
+  destExtensionPairs[taskConfig.dest] = uniq(taskConfig.extensions.concat(destExtensionPairs[taskConfig.dest]))
+})
+
+var assetPaths = []
+for (var dest in destExtensionPairs) {
+  var extensions = destExtensionPairs[dest]
+  var glob = path.join(dest, '/**/*+('+ extensions.join('|') + ')')
+  assetPaths.push(glob)
+}
 
 // 1) Add md5 hashes to assets referenced by CSS and JS files
 gulp.task('rev-assets', function() {
   // Ignore files that may reference assets. We'll rev them next.
-  var ignoreThese = '!' + path.join(config.root.dest,'/**/*+(css|js|json|html)')
-
-  return gulp.src([path.join(config.root.dest,'/**/*'), ignoreThese])
+  return gulp.src(path.join(config.root.dest, '{'+ assetPaths.join(',') +'}'))
     .pipe(rev())
     .pipe(gulp.dest(config.root.dest))
     .pipe(revNapkin({verbose: false}))
