6a3ad08e773865ea274040042f2cfb489c980188
Richard Wu <wrongch@gmail.com>
2016-02-01 17:52:38 +0800

    Fix url issue for iconFont when build on windows platform

--- gulpfile.js/tasks/iconFont/index.js
+++ gulpfile.js/tasks/iconFont/index.js
@@ -7,6 +7,7 @@ var generateIconSass = require('./generateIconSass')
 var handleErrors     = require('../../lib/handleErrors')
 var package          = require('../../../package.json')
 var path             = require('path')
+var url              = require('url')
 
 var fontPath = path.join(config.root.dest, config.tasks.iconFont.dest)
 var cssPath = path.join(config.root.dest, config.tasks.css.dest)
@@ -18,7 +19,7 @@ var settings = {
   sassDest: path.join(config.root.src, config.tasks.css.src, config.tasks.iconFont.sassDest),
   template: path.normalize('./gulpfile.js/tasks/iconFont/template.sass'),
   sassOutputName: '_icons.sass',
-  fontPath: path.relative(cssPath, fontPath),
+  fontPath: url.resolve('.',path.relative(cssPath, fontPath)),
   className: 'icon',
   options: {
     svg: true,
