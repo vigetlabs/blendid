f5d8029cb3653795f6d2ee04db402bcfdbc33e5a
Mats Mikkel Rummelhoff <mail@mmikkel.no>
2016-03-29 01:48:30 +0200

    Fixes issue with middleware where BrowserSync proxy is a string

--- gulpfile.js/tasks/browserSync.js
+++ gulpfile.js/tasks/browserSync.js
@@ -10,19 +10,25 @@ var browserSyncTask = function() {
 
   var webpackConfig = webpackMutiConfig('development')
   var compiler = webpack(webpackConfig)
-  var server = config.tasks.browserSync.server || config.tasks.browserSync.proxy || null
 
-  if (server) {
-    server.middleware = [
-      require('webpack-dev-middleware')(compiler, {
-        stats: 'errors-only',
-        publicPath: '/' + webpackConfig.output.publicPath
-      }),
-      require('webpack-hot-middleware')(compiler)
-    ]
+  var proxy = config.tasks.browserSync.proxy || null;
+  if (typeof(proxy) === 'string') {
+    config.tasks.browserSync.proxy = proxy = {
+      target : proxy
+    }
   }
 
+  var server = proxy || config.tasks.browserSync.server;
+  server.middleware = [
+    require('webpack-dev-middleware')(compiler, {
+      stats: 'errors-only',
+      publicPath: '/' + webpackConfig.output.publicPath
+    }),
+    require('webpack-hot-middleware')(compiler)
+  ]
+
   browserSync.init(config.tasks.browserSync)
+  
 }
 
 gulp.task('browserSync', browserSyncTask)
