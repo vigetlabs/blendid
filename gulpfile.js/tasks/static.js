bf28e657f1e881b53572201451d86af513014e64
Dan Tello <dan.tello@viget.com>
2016-12-13 16:24:35 -0500

    Fix static paths for production output

--- gulpfile.js/tasks/static.js
+++ gulpfile.js/tasks/static.js
@@ -4,17 +4,17 @@ var changed = require('gulp-changed')
 var gulp    = require('gulp')
 var path    = require('path')
 
-var paths = {
-  src: [
-    path.resolve(process.env.PWD, PATH_CONFIG.src, PATH_CONFIG.static.src, '**'),
-    path.resolve(process.env.PWD, '!' + PATH_CONFIG.src, PATH_CONFIG.static.src, 'README.md')
-  ],
-  dest: path.resolve(process.env.PWD, PATH_CONFIG.dest, PATH_CONFIG.static.dest)
-}
 
 var staticTask = function() {
+  var paths = {
+    src: [
+      path.resolve(process.env.PWD, PATH_CONFIG.src, PATH_CONFIG.static.src, '**/*'),
+      path.resolve(process.env.PWD, '!' + PATH_CONFIG.src, PATH_CONFIG.static.src, 'README.md')
+    ],
+    dest: path.resolve(process.env.PWD, PATH_CONFIG.dest, PATH_CONFIG.static.dest)
+  }
+
   return gulp.src(paths.src)
-    .pipe(changed(paths.dest)) // Ignore unchanged files
     .pipe(gulp.dest(paths.dest))
 }
 
