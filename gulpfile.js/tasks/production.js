15262b0348636af809d6c4b9240428412a367a96
Ricardo Schmidt <r.schmidt@queo-group.com>
2017-01-05 14:08:43 +0100

    Fix bug when trying to "cross-link" between multiple devices

--- gulpfile.js/tasks/production.js
+++ gulpfile.js/tasks/production.js
@@ -9,7 +9,9 @@ var productionTask = function(cb) {
 
   // Build to a temporary directory, then move compiled files as a last step
   PATH_CONFIG.finalDest = PATH_CONFIG.dest
-  PATH_CONFIG.dest = path.join(os.tmpdir(), 'gulp-starter')
+  PATH_CONFIG.dest = PATH_CONFIG.temp
+    ? path.join(process.env.PWD, PATH_CONFIG.temp)
+    : path.join(os.tmpdir(), 'gulp-starter');
 
   var tasks = getEnabledTasks('production')
   var rev = TASK_CONFIG.production.rev ? 'rev': false
