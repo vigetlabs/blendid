c83e78541085dd332eff74681279220375071786
Dan Tello <dan.tello@viget.com>
2017-01-16 12:48:05 -0500

    Fix blendid/blendid-karma bin script paths

--- bin/blendid-karma.js
+++ bin/blendid-karma.js
@@ -2,5 +2,5 @@
 var arguments = require('minimist')(process.argv.slice(2));
 require('child_process').fork(
   'node_modules/karma/bin/karma',
-  ['start', __dirname+'/../karma.conf'].concat(arguments._[0])
+  ['start', 'node_modules/blendid/karma.conf'].concat(arguments._[0])
 );
--- bin/blendid.js
+++ bin/blendid.js
@@ -2,5 +2,5 @@
 var arguments = require('minimist')(process.argv.slice(2));
 require('child_process').fork(
   'node_modules/gulp/bin/gulp',
-  ['--gulpfile', __dirname+'/../gulpfile.js'].concat(arguments._[0])
+  ['--gulpfile', 'node_modules/blendid/gulpfile.js'].concat(arguments._[0])
 );
