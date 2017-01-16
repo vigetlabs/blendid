ede59c9132669223b5243b824ec6ae46f8026812
Dan Tello <dan.tello@viget.com>
2017-01-16 13:24:58 -0500

    Fix args in blendid/blendid-karma bin scripts

--- bin/blendid-karma.js
+++ bin/blendid-karma.js
@@ -1,6 +1,10 @@
 #!/usr/bin/env node
-var arguments = require('minimist')(process.argv.slice(2));
-require('child_process').fork(
-  'node_modules/karma/bin/karma',
-  ['start', 'node_modules/blendid/karma.conf'].concat(arguments._[0])
-);
+const arguments = require('minimist')(process.argv.slice(2))
+
+let args = ['start', 'node_modules/blendid/karma.conf']
+
+if(arguments._.length) {
+  args.concat(arguments._)
+}
+
+require('child_process').fork('node_modules/karma/bin/karma', args)
--- bin/blendid.js
+++ bin/blendid.js
@@ -1,6 +1,10 @@
 #!/usr/bin/env node
-var arguments = require('minimist')(process.argv.slice(2));
-require('child_process').fork(
-  'node_modules/gulp/bin/gulp',
-  ['--gulpfile', 'node_modules/blendid/gulpfile.js'].concat(arguments._[0])
-);
+const arguments = require('minimist')(process.argv.slice(2))
+
+let args = ['--gulpfile', 'node_modules/blendid/gulpfile.js']
+
+if(arguments._.length) {
+  args.concat(arguments._)
+}
+
+require('child_process').fork('node_modules/gulp/bin/gulp', args)
