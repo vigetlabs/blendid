1c0285487a261276ce26baeaea9cd2f63a1a3b5c
Dan Tello <dan.tello@viget.com>
2017-01-16 14:03:40 -0500

    Fix args again!

--- bin/blendid.js
+++ bin/blendid.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
-const arguments = require('minimist')(process.argv.slice(2))
+const additionalArgs = require('minimist')(process.argv.slice(2))._
 
 let args = ['--gulpfile', 'node_modules/blendid/gulpfile.js']
 
-if(arguments._.length) {
-  args.concat(arguments._)
+if(additionalArgs.length) {
+  args.concat(additionalArgs)
 }
 
 require('child_process').fork('node_modules/gulp/bin/gulp', args)
