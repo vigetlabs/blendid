ae5777f6e24aabd76a9b4e72af2b3e1b02b27258
Dan Tello <dan.tello@viget.com>
2017-04-28 11:59:51 -0400

    Fix craft init-again

--- gulpfile.js/tasks/init-craft.js
+++ gulpfile.js/tasks/init-craft.js
@@ -7,21 +7,36 @@ gulp.task('init-craft', function() {
   const configStream = gulp.src(['extras/craft/**/*', '*!ASSET-README.md'])
     .pipe(gulp.dest(process.env.PWD))
 
-  const srcStream = gulp.src(['src/**/*', '*.gitkeep', '!src/html', '!src/static'])
+  const srcStream = gulp.src([
+    'src/fonts/**/*',
+    'src/icons/**/*',
+    'src/images/**/*',
+    'src/javascripts/**/*',
+    'src/stylesheets/**/*',
+    ])
     .pipe(gulp.dest(path.join(process.env.PWD, PATH_CONFIG.src)))
 
 
-  gutil.log(gutil.colors.green('Added gulpRev plugin to craft/plugins/gulprev'))
+  gutil.log(gutil.colors.green('Added gulpRev plugin to craft/plugins/gulprev!'))
   gutil.log(
 gutil.colors.magenta(`
+Next Steps
+==========
 
-Make sure to update browserSync.proxy in 'config/task-config.js' to match your development url.
+1) Make sure to update browserSync.proxy in 'config/task-config.js'
+   to match your development url.
 
-`), gutil.colors.magenta(`
-Update the script and stylesheet tags in your layout with the blendid asset helpers:
+2) Enable the Gulp Rev plugin in /admin/settings/plugins
 
-<link rel="stylesheet" href="{{ 'assets/stylesheets/app.css' | gulp_rev }}" />
-<script src="{ 'assets/javascripts/app.js' | gulp_rev }"></script>
+3) Update the script and stylesheet tags in your layout with the
+   blendid asset helpers:
+
+   <link rel="stylesheet" href="{{ 'assets/stylesheets/app.css' | gulp_rev }}" />
+   <script src="{{ 'assets/javascripts/app.js' | gulp_rev }}"></script>
+
+4) Start Compiling!
+
+yarn run blendid
 `))
 
   return mergeStream(configStream, srcStream)
