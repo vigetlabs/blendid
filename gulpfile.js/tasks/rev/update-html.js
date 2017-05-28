52c22626db99ab103c40b2a127270d735c6b1bfb
Eli Van Zoeren <eli@elivz.com>
2017-05-28 08:20:00 -0400

    Run "update-html" task on Twig and Handlebars files, as well as html

--- gulpfile.js/tasks/rev/update-html.js
+++ gulpfile.js/tasks/rev/update-html.js
@@ -7,7 +7,10 @@ var path       = require('path')
 // 5) Update asset references in HTML
 gulp.task('update-html', function(){
   var manifest = gulp.src(path.resolve(process.env.PWD, PATH_CONFIG.dest, "rev-manifest.json"))
-  return gulp.src(path.resolve(process.env.PWD, PATH_CONFIG.dest, PATH_CONFIG.html.dest, '**/*.{' + TASK_CONFIG.html.extensions + '}'))
-    .pipe(revReplace({ manifest: manifest }))
+  return gulp.src(path.resolve(process.env.PWD, PATH_CONFIG.dest, PATH_CONFIG.html.dest, '**/*'))
+    .pipe(revReplace({
+      manifest: manifest,
+      replaceInExtensions: ['.js', '.css', '.html', '.hbs', '.twig']
+    }))
     .pipe(gulp.dest(path.resolve(process.env.PWD, PATH_CONFIG.dest, PATH_CONFIG.html.dest)))
 })
