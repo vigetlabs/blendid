2e2e61c4ceff5324a98f11ac075e6dd00665629a
Smadey <365649304@qq.com>
2017-10-10 21:25:30 +0800

    Fix rev step comment

--- gulpfile.js/tasks/rev/rev-css.js
+++ gulpfile.js/tasks/rev/rev-css.js
@@ -3,7 +3,7 @@ var path      = require('path')
 var rev       = require('gulp-rev')
 var revNapkin = require('gulp-rev-napkin')
 
-// 4) Rev and compress CSS and JS files (this is done after assets, so that if a
+// 3) Rev and compress CSS and JS files (this is done after assets, so that if a
 //    referenced asset hash changes, the parent hash will change as well
 gulp.task('rev-css', function(){
   return gulp.src(path.resolve(process.env.PWD, PATH_CONFIG.dest,'**/*.css'))
