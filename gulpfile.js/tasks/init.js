538c85cdf388f62296bdc7fc1209ec105938530f
benjtinsley <ben.tinsley@viget.com>
2018-01-18 12:42:17 -0700

    references relative path to correct assets and directories to be moved [updates #527]

--- gulpfile.js/tasks/init.js
+++ gulpfile.js/tasks/init.js
@@ -5,13 +5,13 @@ var path = require('path')
 var merge = require('merge-stream')
 
 gulp.task('init', function() {
-  var defaultStream = gulp.src(['extras/default/**/*'])
+  var defaultStream = gulp.src(['../extras/default/**/*'])
     .pipe(gulp.dest(process.env.PWD))
 
-  var configStream = gulp.src(['gulpfile.js/path-config.json', 'gulpfile.js/task-config.js'])
+  var configStream = gulp.src(['./path-config.json', 'gulpfile.js/task-config.js'])
     .pipe(gulp.dest(path.join(process.env.PWD, 'config')))
 
-  var srcStream = gulp.src(['src/**/*', '*.gitkeep'])
+  var srcStream = gulp.src(['../src/**/*', '*.gitkeep'])
     .pipe(gulp.dest(path.join(process.env.PWD, PATH_CONFIG.src)))
 
   log(colors.green('Generating default Blendid project files'))
