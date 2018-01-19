4be18d6380d8cb24aea0e162d728545d7b50f0c9
benjtinsley <ben.tinsley@viget.com>
2018-01-19 15:21:16 -0700

    removes outdated gulp-rev-napkin for one that has been maintained more recently [updates #524]

--- gulpfile.js/tasks/rev/rev-assets.js
+++ gulpfile.js/tasks/rev/rev-assets.js
@@ -1,6 +1,7 @@
 var gulp        = require('gulp')
+var path        = require('path')
 var rev         = require('gulp-rev')
-var revNapkin   = require('gulp-rev-napkin');
+var revDel      = require('gulp-rev-delete-original')
 var projectPath = require('../../lib/projectPath')
 
 // 1) Add md5 hashes to assets referenced by CSS and JS files
@@ -11,7 +12,7 @@ gulp.task('rev-assets', function() {
   return gulp.src([projectPath(PATH_CONFIG.dest,'**/*'), ignoreThese])
     .pipe(rev())
     .pipe(gulp.dest(PATH_CONFIG.dest))
-    .pipe(revNapkin({ verbose: false, force: true }))
+    .pipe(revDel())
     .pipe(rev.manifest(projectPath(PATH_CONFIG.dest, 'rev-manifest.json'), {merge: true}))
     .pipe(gulp.dest(''))
 })
