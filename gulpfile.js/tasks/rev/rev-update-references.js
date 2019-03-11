b248b6bb2690470aa6e310e579632ce35974be16
Henry Bley-Vroman <henry.bley-vroman@viget.com>
2019-03-11 17:43:36 -0400

    chore(dependency): gulp-rev-rewrite replaces gulp-rev-replace

--- gulpfile.js/tasks/rev/rev-update-references.js
+++ gulpfile.js/tasks/rev/rev-update-references.js
@@ -1,5 +1,5 @@
 var gulp        = require('gulp')
-var revReplace  = require('gulp-rev-replace')
+var revRewrite  = require('gulp-rev-rewrite')
 var projectPath = require('../../lib/projectPath')
 
 // 2) Update asset references with reved filenames in compiled css + js
@@ -7,6 +7,6 @@ gulp.task('rev-update-references', function(){
   var manifest = gulp.src(projectPath(PATH_CONFIG.dest, "rev-manifest.json"))
 
   return gulp.src(projectPath(PATH_CONFIG.dest,'**/**.{css,js}'))
-    .pipe(revReplace({manifest: manifest}))
+    .pipe(revRewrite({manifest: manifest}))
     .pipe(gulp.dest(PATH_CONFIG.dest))
 })
--- gulpfile.js/tasks/rev/update-html.js
+++ gulpfile.js/tasks/rev/update-html.js
@@ -1,13 +1,13 @@
 if(!TASK_CONFIG.html) return false
 
 var gulp        = require('gulp')
-var revReplace  = require('gulp-rev-replace')
+var revRewrite  = require('gulp-rev-rewrite')
 var projectPath = require('../../lib/projectPath')
 
 // 4) Update asset references in HTML
 gulp.task('update-html', function(){
   var manifest = gulp.src(projectPath(PATH_CONFIG.dest, "rev-manifest.json"))
   return gulp.src(projectPath(PATH_CONFIG.dest, PATH_CONFIG.html.dest, '**/*.html'))
-    .pipe(revReplace({ manifest: manifest }))
+    .pipe(revRewrite({ manifest: manifest }))
     .pipe(gulp.dest(projectPath(PATH_CONFIG.dest, PATH_CONFIG.html.dest)))
 })
